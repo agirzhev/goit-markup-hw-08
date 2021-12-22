4d5acd8dd0926474be12483600448146a93a10d6
agirzhev <a.girgev@gmail.com>
2021-12-22 13:34:48 +0200

    hw8

--- js/modal.js
+++ js/modal.js
@@ -13,17 +13,3 @@
   }
 })();
 
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector('[data-modal-mobile-open]'),
-    closeModalBtn: document.querySelector('[data-modal-mobile-close]'),
-    modal: document.querySelector('[data-modal-mobile]'),
-  };
-
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
-
-  function toggleModal() {
-    refs.modal.classList.toggle('is-hidden');
-  }
-})();
